352f8970e2630c0636715257c082dbc94f609efa
Tommy Sullivan <tommy@tommysullivan.me>
2026-01-14 19:44:49 +0000

    unit test skips e2e

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/tests/setup.ts"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
   },
 });
