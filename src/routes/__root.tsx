51b228fedb8cf2e042ce509a1c22367a5bb6274f
Tommy Sullivan <tommy@tommysullivan.me>
2026-01-19 18:36:33 +0000

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,6 +27,11 @@ export const Route = createRootRoute({
     ],
     links: [
       {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
         rel: "stylesheet",
         href: appCss,
       },
